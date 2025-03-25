95b156a423cdb1bdefbbda7a5dc8bcab47c3512c
Ayebare Timothy <vexa256@gmail.com>
2025-03-25 10:36:18 +0300

    collate fix

--- config/database.php
+++ config/database.php
@@ -58,7 +58,8 @@ return [
             'strict'         => false,
             'engine'         => null,
             'options'        => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA       => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci",
             ]) : [],
         ],
 
